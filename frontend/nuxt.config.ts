ac0986dbec19a51490cdaf4095e7ac899478b884
Kamil <lekamilthemage@gmail.com>
2025-02-21 16:50:32 +0100

    Frontend: add daisyUI and global css
    With temporary

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/test-utils/module"],
+  modules: ["@nuxt/ui", "@nuxt/test-utils/module", "@nuxtjs/color-mode"],
+  css: ["~/assets/css/main.css"],
+  colorMode: {
+    preference: "light", // "System" doesn't really make sense for a blog; appearance should be consistent for all visitors
+    dataValue: "theme", // Sets data-theme attribute in <html> tag
+    classSuffix: "",
+  },
   compatibilityDate: "2025-02-11"
 })
