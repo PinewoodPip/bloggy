83d33d7f4063d2c398cbca72459462dd67640be4
Kamil <lekamilthemage@gmail.com>
2025-02-22 17:30:10 +0100

    Frontend: fix Nuxt UI color variables conflict with daisyUI

--- frontend/app.vue
+++ frontend/app.vue
@@ -4,3 +4,14 @@
   </UContainer>
   <UNotifications />
 </template>
+
+<script setup lang="ts">
+
+// Workaround for Nuxt UI color CSS variables conflicting with daisyUI ones
+onNuxtReady(() => {
+  // Remove Nuxt UI colors style node.
+  // This contains only color variables, so the rest of the module's styiling is unaffected.
+  window.document.head.querySelector("#nuxt-ui-colors")?.remove()
+})
+
+</script>
