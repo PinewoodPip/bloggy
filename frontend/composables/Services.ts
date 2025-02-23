e63a3c96a163a7afb74b3221107fa6807800521d
Kamil <lekamilthemage@gmail.com>
2025-02-23 18:17:07 +0100

    Frontend services: make service composables share service instances

--- frontend/composables/Services.ts
+++ frontend/composables/Services.ts
@@ -1,7 +1,13 @@
 import UserService from "../services/user"
 
+// Cached service instances;
+// each composable only ever creates one of each service.
+var userService: UserService|null = null
+
 export const useUserService = () => {
-  const config = useRuntimeConfig()
-  const stringifier = new UserService(config.public.API_URL as string)
-  return stringifier
+  if (!userService) {
+    const config = useRuntimeConfig()
+    userService = new UserService(config.public.API_URL as string)
+  }
+  return userService
 }
