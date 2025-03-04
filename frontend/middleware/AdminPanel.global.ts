e048a9048a394030585db4782c1b27ad293fee0c
Kamil <lekamilthemage@gmail.com>
2025-03-04 10:49:53 +0100

    Frontend admin panel: don't run middleware on server
    Since auth checks there are not valid.

--- frontend/middleware/AdminPanel.global.ts
+++ frontend/middleware/AdminPanel.global.ts
@@ -1,6 +1,8 @@
 
 export default defineNuxtRouteMiddleware((to, from) => {
+  if (import.meta.server) return // Don't run this middleware on server
   const userService = useUserService()
+  
   // Redirect to login page if accessing admin panel while not logged in
   if (to.path.startsWith("/admin") && !to.path.endsWith("login") && !userService.isLoggedIn()) {
     return navigateTo("/admin/login")
