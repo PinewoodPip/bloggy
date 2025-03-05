797c0af6016dc03d7ce334f6fa8689c84c69f67e
Kamil <lekamilthemage@gmail.com>
2025-03-05 07:31:52 +0100

    Frontend editor: disallow alt key in shortcuts

--- frontend/composables/ArbitraryKeyShortcuts.ts
+++ frontend/composables/ArbitraryKeyShortcuts.ts
@@ -8,16 +8,19 @@ let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 let symbols = ';\'\",./?=+`\\'.split('')
 let specialKeys = [' ', 'enter', 'tab']
 let allKeys = [...alphabet, ...symbols, ...specialKeys]
-let modifiers = ['ctrl', 'alt', 'shift']
+let modifiers = ['ctrl', 'shift'] // 'alt' appears to be unusable in major browsers.
 
 export const useArbitraryKeyshortcuts = (handler: (keybind: string) => void, whenever?: (keys: string) => boolean) => {
+  
   let shortcutEntries: {[key: string]: object} = {} // TODO import type - but from where?
   for (const modifier of modifiers) {
     for (const key of allKeys) {
       const nuxtKeyComboID = modifier === '' ? key : `${modifier}_${key}`
       const shortcutEntry = {
         usingInput: true,
-        whenever: whenever ? [() => {return whenever(nuxtKeyComboID)}] : [],
+        whenever: whenever ? [() => {
+          return whenever(nuxtKeyComboID)
+        }] : [],
         handler: () => {
           handler(nuxtKeyComboID)
         }, 
