521127de322f46fc3967d4f751bb14faa3a5d747
Kamil <lekamilthemage@gmail.com>
2025-03-13 08:27:56 +0100

    Frontend MutationButton: replace icon with loading spinner while the mutation is pending

--- frontend/components/MutationButton.vue
+++ frontend/components/MutationButton.vue
@@ -1,8 +1,8 @@
 <!-- A button associated with a vue-query mutation; will show a loading spinner while the mutation is pending. Slot corresponds to button content. -->
 <template>
-  <IconButton class="btn-primary" :icon="icon" :disabled="status === 'pending' || disabled">
-    <span v-if="status === 'pending'" class="loading loading-spinner" />
-    <span v-else><slot/></span>
+  <IconButton class="btn-primary" :icon="status !== 'pending' ? icon : ''" :disabled="status === 'pending' || disabled">
+    <span v-if="status === 'pending'" class="loading loading-spinner loading-xs" />
+    <span><slot/></span>
   </IconButton>
 </template>
 
