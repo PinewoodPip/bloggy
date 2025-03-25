8e9439c06807db71056ed1c29dc16b56fa6db603
Kamil <lekamilthemage@gmail.com>
2025-03-25 10:19:01 +0100

    Frontend: add server route for files

---
+++ frontend/server/routes/files/[...file_path].get.ts
@@ -0,0 +1,38 @@
+/**
+ * Route for mapping files in the backend to frontend URLs.
+ */
+import { AxiosError } from "axios";
+import { useFileService } from "~/composables/Services";
+
+/** Maps extensions to their common HTTP application type equivalent suffix. */
+const EXTENSION_ALIASES: {[key: string]: string} = {
+  jpg: 'jpeg'
+}
+
+export default defineEventHandler(async (event) => {
+  const fileService = useFileService()
+  const path = event.context.params!.file_path
+
+  // Fetch file and decode response
+  try {
+    const file = await fileService.getFile('/' + path)
+
+    // Read extension and convert to a commmon HTTP application type ID
+    let extension = file.filename.split('.')[1]
+    extension = EXTENSION_ALIASES[extension] || extension
+
+    // Decode response
+    const decodedContent = Buffer.from(file.content, 'base64')
+    
+    // Set headers
+    setResponseHeader(event, 'Content-Type', 'image/' + extension) // TODO detect non-images
+    setResponseHeader(event, 'Content-Length', decodedContent.length)
+
+    return decodedContent;
+  } catch (e: any) {
+    // Propagate backend response
+    const err = e as AxiosError
+    setResponseStatus(event, err.status, err.message)
+    return
+  }
+});
