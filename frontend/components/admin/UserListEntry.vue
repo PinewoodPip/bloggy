34a2b0225e9d2beb7b83eed99ee8986f3694b943
Kamil <lekamilthemage@gmail.com>
2025-03-28 10:22:58 +0100

    Frontend users panel: hide articles button for admin accounts, adjust styling

--- frontend/components/admin/UserListEntry.vue
+++ frontend/components/admin/UserListEntry.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="flex items-center hover:bg-secondary/50 rounded-btn pl-2">
+  <div class="flex items-center bg-secondary/20 hover:bg-secondary/50 rounded-btn p-2">
     <!-- Avatar TODO -->
     <AvatarIcon class="size-8 mr-2"/>
 
     <!-- Name displays; admins have no displayname, so display their role instead -->
-    <span>{{ user.role === "editor" ? user.display_name : user.username }}</span>
+    <span class="mr-1">{{ user.role === "editor" ? user.display_name : user.username }}</span>
     <span class="italic">({{ user.role === "editor" ? user.username : "admin" }})</span>
 
     <div class="mx-auto"/>
 
     <!-- Management buttons -->
     <div class="flex gap-x-2">
-      <IconButton icon="i-heroicons-book-open" class="btn-smp btn-secondary" @click="showArticles">View articles</IconButton>
-      <IconButton v-if="editable" icon="i-heroicons-pencil" class="btn-smp btn-secondary" @click="editAccount">Edit account</IconButton>
+      <IconButton v-if="isEditor" icon="i-heroicons-book-open" class="btn-sm btn-secondary" @click="showArticles">View articles</IconButton>
+      <IconButton v-if="editable" icon="i-heroicons-pencil" class="btn-sm btn-secondary" @click="editAccount">Edit account</IconButton>
     </div>
   </div>
 </template>
@@ -35,5 +35,9 @@ function showArticles() {
 function editAccount() {
   emit('edit', props.user)
 }
+
+const isEditor = computed(() => {
+  return props.user.role === 'editor'
+})
 
 </script>
