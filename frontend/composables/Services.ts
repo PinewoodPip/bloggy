2896d9adaa1c6876439864c72782dfae549f4846
Kamil <lekamilthemage@gmail.com>
2025-04-01 09:25:03 +0200

    Frontend services: refactor composables to reduce code duplication

--- frontend/composables/Services.ts
+++ frontend/composables/Services.ts
@@ -3,51 +3,34 @@ import CategoryService from "~/services/category"
 import ArticleService from "~/services/article"
 import FileService from "~/services/file"
 import SearchService from "~/services/search"
+import type Service from "~/services/service"
 
 // Cached service instances;
 // each composable only ever creates one of each service.
-var userService: UserService|null = null
-var categoryService: CategoryService|null = null
-var articleService: ArticleService|null = null
-var fileService: FileService|null = null
-var searchService: SearchService|null = null
+var serviceInstances: {[className: string]: Service} = {}
+
+/** Pooled service composable. */
+export const useService = (id: string, serviceClass: new (apiURL: string) => Service) => {
+  if (!(id in serviceInstances)) {
+    const config = useRuntimeConfig()
+    const instance = new serviceClass(config.public.API_URL)
+    serviceInstances[id] = instance
+  }
+  return serviceInstances[id]
+}
 
 export const useUserService = () => {
-  if (!userService) {
-    const config = useRuntimeConfig()
-    userService = new UserService(config.public.API_URL as string)
-  }
-  return userService
+  return useService("User", UserService) as UserService
 }
-
 export const useCategoryService = () => {
-  if (!categoryService) {
-    const config = useRuntimeConfig()
-    categoryService = new CategoryService(config.public.API_URL as string)
-  }
-  return categoryService
+  return useService("Category", CategoryService) as CategoryService
 }
-
 export const useArticleService = () => {
-  if (!articleService) {
-    const config = useRuntimeConfig()
-    articleService = new ArticleService(config.public.API_URL as string)
-  }
-  return articleService
+  return useService("Article", ArticleService) as ArticleService
 }
-
 export const useFileService = () => {
-  if (!fileService) {
-    const config = useRuntimeConfig()
-    fileService = new FileService(config.public.API_URL as string)
-  }
-  return fileService
+  return useService("File", FileService) as FileService
 }
-
 export const useSearchService = () => {
-  if (!searchService) {
-    const config = useRuntimeConfig()
-    searchService = new SearchService(config.public.API_URL as string)
-  }
-  return searchService
+  return useService("Search", SearchService) as SearchService
 }
