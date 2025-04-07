84a7d1aa2b359eabe4f3c4bcf20672ec727e4fb1
Kamil <lekamilthemage@gmail.com>
2025-04-07 09:25:05 +0200

    Frontend site: fetch favicon from backend

--- frontend/app.vue
+++ frontend/app.vue
@@ -4,12 +4,20 @@
 </template>
 
 <script setup lang="ts">
+import { useFavicon } from '@vueuse/core'
 
 // Workaround for Nuxt UI color CSS variables conflicting with daisyUI ones
 onNuxtReady(() => {
   // Remove Nuxt UI colors style node.
   // This contains only color variables, so the rest of the module's styiling is unaffected.
   window.document.head.querySelector("#nuxt-ui-colors")?.remove()
+})
+
+// Set favicon
+const favicon = useFavicon()
+const runtimeConfig = useRuntimeConfig()
+onMounted(() => {
+  favicon.value = runtimeConfig.public.API_URL + 'site/favicon'
 })
 
 </script>
