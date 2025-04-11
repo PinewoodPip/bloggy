14de8e7eeaee3ee41345d3a77d2380220f68e6a9
Kamil <lekamilthemage@gmail.com>
2025-04-11 07:47:08 +0200

    Frontend site: set theme from config

--- frontend/app.vue
+++ frontend/app.vue
@@ -6,18 +6,33 @@
 <script setup lang="ts">
 import { useFavicon } from '@vueuse/core'
 
+const config = useSiteConfig()
+const favicon = useFavicon()
+const runtimeConfig = useRuntimeConfig()
+
 // Workaround for Nuxt UI color CSS variables conflicting with daisyUI ones
 onNuxtReady(() => {
   // Remove Nuxt UI colors style node.
   // This contains only color variables, so the rest of the module's styiling is unaffected.
   window.document.head.querySelector("#nuxt-ui-colors")?.remove()
+  
+  // Set theme if config is loaded before initialization
+  if (config.data.value) {
+    colorMode.preference = config.data.value.theme
+  }
 })
 
 // Set favicon
-const favicon = useFavicon()
-const runtimeConfig = useRuntimeConfig()
 onMounted(() => {
   favicon.value = runtimeConfig.public.apiUrl + 'site/favicon'
+})
+
+// Set theme when config loads
+const colorMode = useColorMode()
+watchEffect(() => {
+  if (config.data.value) {
+    colorMode.preference = config.data.value.theme
+  }
 })
 
 </script>
