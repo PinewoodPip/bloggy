558c4afc37075f7598fcf77c003e9ff4c7f583cd
Kamil <lekamilthemage@gmail.com>
2025-04-11 08:31:52 +0200

    Frontend site: implement sidebar

--- frontend/components/site/Sidebar.vue
+++ frontend/components/site/Sidebar.vue
@@ -1,7 +1,31 @@
 <!-- Sidebar for published site pages. -->
 <template>
-  <div class="large-content-block min-w-80">
-    <h2>Sidebar</h2>
-    <p>TODO</p>
+  <div v-if="sidebar" class="large-content-block max-w-80">
+    <!-- Will switch from markdown to rich document after mount -->
+    <SiteArticleContent v-if="mounted" :initial-content="sidebar?.content" />
+    <MarkdownDocument v-else :content="sidebar.content" />
   </div>
 </template>
+
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query'
+
+const siteService = useSiteService()
+
+const mounted = ref(false)
+
+/** Tracks mounted state. */
+onMounted(() => {
+  mounted.value = true
+})
+
+/** Query for the sidebar document. */
+const { data: sidebar, status: sidebarStatus, suspense: sidebarSuspense } = useQuery({
+  queryKey: ['sidebarDocument'],
+  queryFn: async () => {
+    return await siteService.getSidebar()
+  },
+})
+await sidebarSuspense(); // Have the server wait for the request to resolve
+
+</script>
