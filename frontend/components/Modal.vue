aab0abe262bb1f5025e5aada50599b257b686121
Kamil <lekamilthemage@gmail.com>
2025-04-26 17:50:38 +0200

    Frontend: add scrolling to modals

--- frontend/components/Modal.vue
+++ frontend/components/Modal.vue
@@ -1,8 +1,8 @@
 <!-- A standard modal component with a header, form content area, and optional footer. -->
 <template>
-  <div class="large-modal">
+  <div class="large-modal relative">
     <!-- Header -->
-    <div class="flex content-center pb-2">
+    <div class="flex sticky top-0 content-center bg-white border-b border-b-neutral/50 z-10 pb-2">
       <slot name="headerTitle"/>
 
       <HorizontalFill/>
@@ -12,12 +12,12 @@
     </div>
 
     <!-- Form -->
-    <div class="flexcol gap-y-2 py-3">
+    <div class="flexcol flex-grow overflow-y-scroll gap-y-2 py-3">
       <slot name="form"/>
     </div>
 
     <!-- Footer; optional -->
-    <div v-if="$slots.footer" class="flex justify-center gap-x-2 pt-4">
+    <div v-if="$slots.footer" class="flex sticky bottom-0 justify-center gap-x-2 bg-white border-t border-t-neutral/50 pt-4">
       <slot name="footer"/>
     </div>
   </div>
