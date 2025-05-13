2006edccf7930b2bbdb8b3642d2023f7fe091187
Kamil <lekamilthemage@gmail.com>
2025-05-13 08:18:49 +0200

    Frontend editor: fix links not working in read-only mode

--- frontend/components/editor/mark/Link.vue
+++ frontend/components/editor/mark/Link.vue
@@ -1,13 +1,15 @@
 <!-- Mark view for links. -->
 <template>
   <UTooltip :text="mark.attrs.title">
-    <span class="link link-secondary" :ref="contentRef"/>
+    <!-- Links are only clickable when the editor is in read-only mode -->
+    <a class="link link-secondary" :href="!editorView || !editorView.editable ? mark.attrs.href : '#'" :ref="contentRef"/>
   </UTooltip>
 </template>
 
 <script setup lang="ts">
 import { useMarkViewContext } from '@prosemirror-adapter/vue'
 
+const { editorView } = useEditorInjects()
 const { mark, contentRef } = useMarkViewContext()
 
 </script>
