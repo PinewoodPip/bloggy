1c2624f12bfcae5a9ea6c2b36cff67a780464da3
Kamil <lekamilthemage@gmail.com>
2025-05-13 16:02:38 +0200

    Frontend admin panel: improve config panel styling

--- frontend/pages/admin/config.client.vue
+++ frontend/pages/admin/config.client.vue
@@ -1,52 +1,54 @@
 <template>
   <AdminPage header="Content" icon="material-symbols:settings-rounded" hint="Manage site-wide configuration.">
+    <div class="flexcol overflow-y-auto">
+      <!-- Branding -->
+      <h2 class="mt-2">Branding</h2>
+      <div class="flex gap-x-5">
+        <div class="flexcol flex-grow gap-y-2">
+          <!-- Site name -->
+          <FormGroupInputField v-model="configUpdate.site_name" label="Site name" hint="Shown in the browser's title bar." icon="material-symbols:drive-file-rename-outline" />
 
-    <!-- Branding -->
-    <h2 class="mt-2">Branding</h2>
-    <div class="flex gap-x-5">
-      <div class="flexcol flex-grow gap-y-2">
-        <!-- Site name -->
-        <FormGroupInputField v-model="configUpdate.site_name" label="Site name" hint="Shown in title bar." icon="material-symbols:drive-file-rename-outline" />
+          <!-- Favicon -->
+          <FormGroupSiteFile v-model="configUpdate.favicon_path" label="Favicon" hint="Icon shown by the browser tab." icon="material-symbols:star" />
+        </div>
+        <div class="flexcol flex-grow max-w-md">
+          <FormGroupSiteImage v-model="configUpdate.logo_path" label="Logo" />
+        </div>
+      </div>
 
-        <!-- Favicon -->
-        <FormGroupSiteFile v-model="configUpdate.favicon_path" label="Favicon" hint="Icon shown by the browser tab." icon="material-symbols:star" />
+      <FaintHr class="my-2" />
+
+      <!-- Settings -->
+      <div v-if="configStatus === 'success'" class="flex-grow">
+        <!-- Theming -->
+        <FormGroupMultiselect v-model="configUpdate.theme" :options="themes" :multiple="false" label="Theme" help="Determines the site's color scheme." :show-labels="true" :searchable="true" placeholder="Select theme" aria-label="select theme" @update:model-value="onThemeChanged" />
+
+        <FaintHr class="my-2" />
         
-        <!-- Sidebar -->
-        <!-- <FormGroupArticle v-model="configUpdate.sidebar_document" type="article" label="Sidebar document" hint="Contents of the site's sidebar." icon="material-symbols:thumbnail-bar" /> -->
+        <!-- Sidebar editor -->
+        <h2>Sidebar</h2>
+        <p>Leave blank to remove the sidebar.</p>
+        <AdminSidebarEditor ref="sidebarEditorRef" class="mt-2" />
+
+        <FaintHr class="my-2" />
+
+        <!-- Social networks -->
+        <h2>Social Media Sharing</h2>
+        <p>Select the social media networks that sharing options will be available for in article pages.</p>
+        <div class="flexcol gap-y-1 mt-1">
+          <FormGroupCheckbox v-for="network in sortedNetworks" v-model="enabledNetworks[network.id]" :icon="NETWORK_ICONS[network.id] || 'material-symbols:question-mark'" :label="network.name" />
+        </div>
       </div>
-      <div class="flexcol flex-grow">
-        <FormGroupSiteImage v-model="configUpdate.logo_path" label="Logo" />
-      </div>
-    </div>
+      <LoadingSpinner v-else />
 
-    <FaintHr class="my-2" />
+      <!-- Bottom padding -->
+      <div class="my-4" />
 
-    <!-- Settings -->
-    <div v-if="configStatus === 'success'" class="flex-grow">
-      <!-- Theming -->
-      <FormGroupMultiselect v-model="configUpdate.theme" :options="themes" :multiple="false" label="Theme" help="Determines the site's color scheme." :show-labels="true" :searchable="true" placeholder="Select theme" aria-label="select theme" @update:model-value="onThemeChanged" />
-
-      <FaintHr class="my-2" />
-      
-      <!-- Sidebar editor -->
-      <h2>Sidebar</h2>
-      <p>Leave blank to remove the sidebar.</p>
-      <AdminSidebarEditor ref="sidebarEditorRef" class="mt-2" />
-
-      <FaintHr class="my-2" />
-
-      <!-- Social networks -->
-      <h2>Social Media Sharing</h2>
-      <p>Select the social media networks that sharing options will be available for in article pages.</p>
-      <div class="flexcol">
-        <FormGroupCheckbox v-for="network in sortedNetworks" v-model="enabledNetworks[network.id]" :icon="NETWORK_ICONS[network.id] || 'material-symbols:question-mark'" :label="network.name" />
+      <!-- Save button; sticks to bottom of viewport -->
+      <div class="flex sticky bottom-0 border-t border-neutral/50 p-2 w-100 bg-base-200 top-shadow">
+        <MutationButton class="max-w-lg mx-auto" icon="material-symbols:save" :status="patchStatus" @click="applyChanges">Save</MutationButton>
       </div>
     </div>
-    <LoadingSpinner v-else />
-
-    <FaintHr class="my-2" />
-
-    <MutationButton class="max-w-lg mx-auto" icon="material-symbols:save" :status="patchStatus" @click="applyChanges">Save</MutationButton>
   </AdminPage>
 </template>
 
@@ -86,6 +88,7 @@ function applyChanges() {
 
 // Initialize models when config is fetched
 function initializeModel() {
+  configUpdate.site_name = config.value?.site_name
   configUpdate.logo_path = config.value?.logo?.path
   configUpdate.favicon_path = config.value?.favicon?.path
   configUpdate.theme = config.value?.theme
@@ -127,3 +130,11 @@ const { mutate: requestPatch, status: patchStatus } = useMutation({
 })
 
 </script>
+
+<style scoped>
+.top-shadow {
+  box-shadow: 2px -9px 25px -3px rgba(0,0,0,0.75);
+  -webkit-box-shadow: 2px -9px 25px -3px rgba(0,0,0,0.75);
+  -moz-box-shadow: 2px -9px 25px -3px rgba(0,0,0,0.75);
+}
+</style>
