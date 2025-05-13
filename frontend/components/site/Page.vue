0852f9aabcc7665a41584e0389dc0d301bf6836d
Kamil <lekamilthemage@gmail.com>
2025-05-13 16:02:16 +0200

    Frontend site: show site name as fallback if logo fails to load or is unset

--- frontend/components/site/Page.vue
+++ frontend/components/site/Page.vue
@@ -1,9 +1,10 @@
 <!-- Base component for published site pages; includes header, navigation bar and footer. -->
 <template>
   <UContainer class="flexcol gap-y-5 items-center min-h-screen pt-5">
-    <figure class="placeholder bg-base-300 h-36 cursor-pointer">
+    <figure class="h-36 cursor-pointer">
       <RouterLink to="/">
-        <img class="object-contain h-full" :src="logoPath" alt="Site logo" />
+        <img v-if="!logoLoadingFailed" class="object-contain h-full" :src="logoPath" @error="onLogoLoadingFailed" alt="Site logo" />
+        <p class="text-xl" v-else>{{ siteConfig.data.value?.site_name }}</p> <!-- Display site name as fallback -->
       </RouterLink>
     </figure>
 
@@ -33,12 +34,20 @@
 <script setup lang="ts">
 
 const runtimeConfig = useRuntimeConfig()
+const siteConfig = useSiteConfig()
 const navigation = useNavigationBar()
+
+const logoLoadingFailed = ref(false)
 
 const { title, showSidebar = true } = defineProps<{
   title: string,
   showSidebar?: boolean,
 }>()
+
+/** Sets a flag to display the site's name as a fallback instead of the logo. */
+function onLogoLoadingFailed() {
+  logoLoadingFailed.value = true
+}
 
 const logoPath = computed(() => {
   return runtimeConfig.public.apiUrl + 'site/logo'
