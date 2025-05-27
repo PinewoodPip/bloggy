6668c9b89b74ed67d242cdabbcf186264f253c81
Kamil <lekamilthemage@gmail.com>
2025-05-27 09:08:04 +0200

    Frontend editor: close context menu when clicking outside or pressing esc
    Polishing after user tests.

--- frontend/components/context-menu/ContextMenu.vue
+++ frontend/components/context-menu/ContextMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <UContextMenu v-model="model" :virtual-element="virtualElement">
+  <UContextMenu v-model="model" :virtual-element="virtualElement" @click.stop="">
     <Dropdown data-nuxtui-contextmenu-dropdown :items="items" v-model:open="model" :popper="{ placement: 'bottom-start' }">
       <div></div>
     </Dropdown>
@@ -12,6 +12,12 @@ import { useMouse, useWindowScroll } from '@vueuse/core'
 const { x, y } = useMouse()
 const { y: windowY } = useWindowScroll()
 const virtualElement = ref({ getBoundingClientRect: () => ({}) })
+const { editorView, editorState } = useEditorInjects()
+
+watch(editorState, () => {
+  if (!editorState.value) return
+  model.value = false
+})
 
 const props = defineProps<{
   items: object[][], // TODO annotate
@@ -19,6 +25,17 @@ const props = defineProps<{
 
 const model = defineModel({
   default: false,
+})
+
+function onClick() {
+  model.value = false
+}
+
+onMounted(() => {
+  document.addEventListener('click', onClick)
+})
+onUnmounted(() => {
+  document.removeEventListener('click', onClick)
 })
 
 // Position the context menu when it is opened
@@ -33,6 +50,17 @@ watch(model, () => {
     top,
     left
   })
+})
+
+// Escape shortcut to close
+defineShortcuts({
+  escape: {
+    usingInput: true,
+    whenever: [model],
+    handler: () => {
+      model.value = false
+    },
+  }
 })
 
 </script>
